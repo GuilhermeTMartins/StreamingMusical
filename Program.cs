f978310d58e6e43085510816879c939d56402cd2
Guilherme Tesch Martins <guilhermeteschmartins@gmail.com>
2025-06-04 12:55:18 -0300

    Criando Menu

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿// Screen Sound
+string boasVindas = "Boas vindas ao Screen Sound";
+
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.WriteLine(boasVindas);
+}
+
+void ExibirOpcoesMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a media de uma banda");
+    Console.WriteLine("Digite 5 para sair");
+
+    Console.Write("\nDigite sua opcao: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNum = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNum)
+    {
+        case 1:
+            RegistrarBandas();
+            break;
+        case 2:
+            Console.WriteLine("Voce escolheu a opcao " + opcaoEscolhidaNum);
+            break;
+        case 3:
+            Console.WriteLine("Voce escolheu a opcao " + opcaoEscolhidaNum);
+            break;
+        case 4:
+            Console.WriteLine("Voce escolheu a opcao " + opcaoEscolhidaNum);
+            break;
+        case 5:
+            Console.WriteLine("Ate logo :3");
+            break;
+        default: Console.WriteLine("Opcao invalida");
+            break;
+    }
+}
+
+void RegistrarBandas()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de Bandas");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    Console.WriteLine($"A banda foi {nomeDaBanda} com sucesso");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesMenu();
+}
+
+ExibirLogo();
+ExibirOpcoesMenu();
